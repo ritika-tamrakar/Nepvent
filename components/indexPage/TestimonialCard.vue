e495489238e4ef5e246ebc3de0af0f150c165294
rjan-sunar <arjan.sunar@logisparktech.com>
2022-05-24 16:20:03 +0545

    creating testimonials section

--- components/indexPage/TestimonialCard.vue
+++ components/indexPage/TestimonialCard.vue
@@ -1,8 +1,26 @@
 <template>
-  <div>{{ clientName }}</div>
+  <div class="card-wrapper">
+    <div class="content-wrapper">
+      <BareQuotes />
+      <p class="testimonial">
+        {{ testimonial }}
+      </p>
+      <div class="text-wrapper">
+        <div class="review-wrapper">
+          <StarSvg v-for="(_, index) in Array(noOfStars)" :key="index" />
+        </div>
+        <h3 class="client-name">{{ clientName }}</h3>
+        <p class="business-name">{{ businessName }}</p>
+      </div>
+    </div>
+
+    <span class="business-name-decor">{{ businessName }}</span>
+  </div>
 </template>
 
 <script>
+import BareQuotes from './BareQuotes.vue'
+import StarSvg from './StarSvg.vue'
 export default {
   props: {
     clientName: {
@@ -17,8 +35,30 @@ export default {
       type: String,
       default: '',
     },
+    noOfStars: {
+      type: Number,
+      default: 5,
+    },
   },
+  components: { BareQuotes, StarSvg },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-wrapper {
+  margin-top: 1.9rem;
+  background-color: map-get($map: $colors, $key: light-300);
+
+  .content-wrapper {
+    padding: 1.5rem 1.6rem;
+
+    /* media queries */
+    @include mq(xs) {
+      padding: 1.5rem 2rem;
+    }
+    @include mq(md) {
+      padding: 2.4rem 4.125rem;
+    }
+  }
+}
+</style>
--- components/indexPage/TestimonialsSection.vue
+++ components/indexPage/TestimonialsSection.vue
@@ -2,7 +2,9 @@
   <section class="testimonials-container">
     <h6 class="section-tag">testimonials</h6>
     <h3 class="section-title">Check what our <br />clients are saying</h3>
-    <div class="testimonial-wrapper">
+
+    <div class="testimonials-wrapper">
+      <QuotesSvg class="quotes-svg" />
       <TestimonialCard
         v-for="(
           { clientName, businessName, testimonial }, index
@@ -18,8 +20,9 @@
 
 <script>
 import TestimonialCard from './TestimonialCard.vue'
+import QuotesSvg from './QuotesSvg.vue'
 export default {
-  components: { TestimonialCard },
+  components: { TestimonialCard, QuotesSvg },
   data() {
     return {
       testimonials: [
@@ -38,6 +41,42 @@ export default {
 <style lang="scss" scoped>
 .testimonials-container {
   @include container();
-  padding: 2rem 1rem;
+
+  padding: 2rem var(--horizontal-padding);
+
+  .section-tag {
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 19px;
+  }
+  .section-title {
+    margin-top: 0.9rem;
+    font-weight: 800;
+    font-size: 32px;
+    line-height: 40px;
+    /* or 120% */
+
+    letter-spacing: -0.03em;
+  }
+
+  @media (min-width: map-get($map: $breakpoints, $key: md)) {
+    .section-tag {
+      font-size: 14px;
+    }
+    .section-title {
+      font-size: 40px;
+      line-height: 48px;
+    }
+  }
+
+  .testimonials-wrapper {
+    @include mq(xs) {
+      padding: 0 1rem;
+    }
+  }
+  .quotes-svg {
+    margin-top: 2rem;
+  }
 }
 </style>
